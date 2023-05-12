e8af648ffd315b5a92683063a7f507c88260d647
eliaslassalle <eliaslassalle@gmail.com>
2023-05-12 01:57:37 +0100

    Push!

---
+++ labs/Lab5/scripts/exampleScripts/helloworld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title> Hello World</title>
+</head>
+
+<p>HTML says hello World</p>
+
+<script type = "text/javascript">
+    document.write('<p>JavaScript says Hello World</p>');
+</script>
+
+<?php
+echo "<p>PHP says hello World</p>";
+?>
+
+<body>
+</body>
+</html>
